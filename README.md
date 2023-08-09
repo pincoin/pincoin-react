d5b1444d31248b8502bd79a5479afdc3235fca6c
pincoin <pincoins@gmail.com>
2023-08-09 22:05:33 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# 개발환경
+
+* 리액트/ES6
+* 의존성
+    * react-router-dom
+    * tailwind
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const prod = process.env.NODE_ENV === "production";
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const dotenv = require("dotenv");
+const webpack = require("webpack");
+dotenv.config(); // .env 파일 파싱한 결과 객체를 process.env에 저장
+
+module.exports = {
+    mode: prod ? "production" : "development",
+    devtool: prod ? undefined : "source-map",
+
+    entry: "./src/index.js",
+    output: {
+        path: __dirname + "/dist/",
+        publicPath: "/", // react-router-dom 설정 필수
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif|svg|webp)$/i,
+                use: ["file-loader"],
+            },
+            {
+                test: /\.(js|jsx)$/i,
+                exclude: /node_modules/,
+                resolve: {
+                    extensions: [".js", ".jsx", ".json"],
+                },
+                use: ["babel-loader"],
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "public/index.html",
+            title: process.env.SITE_TITLE,
+        }),
+        new MiniCssExtractPlugin(),
+        new webpack.DefinePlugin({ // dotenv 플러그인 정의
+            'process.env': JSON.stringify(process.env),
+        }),
+    ],
+    devServer: {
+        port: 3000,
+        hot: true,
+        compress: true,
+        historyApiFallback: true, // react-router-dom 설정 필수
+        open: true,
+    },
+};
