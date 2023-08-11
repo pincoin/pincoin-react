79d564b4c1f9dfbbe5bd53e228ee56f876085f3a
pincoin <pincoins@gmail.com>
2023-08-11 11:21:12 +0900

    dropdown menu

--- src/components/NavbarDesktop.js
+++ src/components/NavbarDesktop.js
@@ -1,7 +1,144 @@
+import { Menu } from '@headlessui/react';
 import React from 'react';
-import {MdInfoOutline, MdLogin, MdLogout, MdPersonPin, MdSendToMobile, MdShoppingBag} from 'react-icons/md';
+import {
+  MdInfoOutline,
+  MdLogin,
+  MdLogout,
+  MdPersonPin,
+  MdSendToMobile,
+  MdShoppingBag,
+} from 'react-icons/md';
 import { Link } from 'react-router-dom';
 import ContainerFixed from '../ui/layouts/ContainerFixed';
+import DropdownMenu from './DropdownMenu';
+
+const menu = [
+  {
+    id: 1,
+    title: '구글/넥슨/퍼니카드',
+    children: [
+      {
+        id: 1,
+        title: '구글기프트카드',
+        slug: '구글기프트카드',
+      },
+      {
+        id: 2,
+        title: '넥슨카드',
+        slug: '넥슨카드',
+      },
+      {
+        id: 1,
+        title: '퍼니카드',
+        slug: '퍼니카드',
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: '스마트/도서문화/컬쳐랜드',
+    children: [
+      {
+        id: 1,
+        title: '컬쳐랜드상품권',
+        slug: '컬쳐랜드상품권',
+      },
+      {
+        id: 2,
+        title: '문화상품권',
+        slug: '문화상품권',
+      },
+      {
+        id: 3,
+        title: '도서문화상품권',
+        slug: '도서문화상품권',
+      },
+      {
+        id: 4,
+        title: '스마트문화상품권',
+        slug: '스마트문화상품권',
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: '에그/해피머니',
+    children: [
+      {
+        id: 1,
+        title: '해피머니',
+        slug: '해피머니',
+      },
+      {
+        id: 2,
+        title: '에그머니',
+        slug: '에그머니',
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: '온/틴캐시',
+    children: [
+      {
+        id: 1,
+        title: '온캐시',
+        slug: '온캐시',
+      },
+      {
+        id: 2,
+        title: '틴캐시',
+        slug: '틴캐시',
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: '선불쿠폰',
+    children: [
+      {
+        id: 1,
+        title: '요기요',
+        slug: '요기요',
+      },
+      {
+        id: 2,
+        title: '아프리카별풍선',
+        slug: '아프리카별풍선',
+      },
+      {
+        id: 3,
+        title: '한게임상품권',
+        slug: '한게임상품권',
+      },
+      {
+        id: 4,
+        title: 'N코인',
+        slug: '엔코인',
+      },
+      {
+        id: 5,
+        title: '와우캐시',
+        slug: '와우캐시',
+      },
+      {
+        id: 6,
+        title: '플레이스테이션',
+        slug: '플레이스테이션',
+      },
+      {
+        id: 7,
+        title: '매니아선불쿠폰',
+        slug: '매니아선불쿠폰',
+      },
+      {
+        id: 8,
+        title: '아이템베이선불쿠폰',
+        slug: '아이템베이선불쿠폰',
+      },
+    ],
+  },
+];
 
 const NavbarDesktop = () => {
   return (
@@ -24,15 +161,15 @@ const NavbarDesktop = () => {
                 <span>고객센터</span>
               </Link>
               <Link to="/" className="inline-flex items-center gap-x-1">
-                <MdPersonPin/>
+                <MdPersonPin />
                 <span>마이페이지</span>
               </Link>
               <Link to="/" className="inline-flex items-center gap-x-1">
-                <MdLogin/>
+                <MdLogin />
                 <span>로그인</span>
               </Link>
               <Link to="/" className="inline-flex items-center gap-x-1">
-                <MdLogout/>
+                <MdLogout />
                 <span>로그아웃</span>
               </Link>
             </div>
@@ -42,11 +179,25 @@ const NavbarDesktop = () => {
       <div className="bg-green-50 text-green-950">
         <ContainerFixed className="px-2 py-2">
           <div className="flex gap-x-4">
-            <div>구글/넥슨/퍼니카드</div>
-            <div>스마트/도서문화/컬쳐랜드</div>
-            <div>에그/해피머니</div>
-            <div>온/틴캐시</div>
-            <div>선불쿠폰</div>
+            {menu.map((menuItem) => {
+              return (
+                <DropdownMenu title={menuItem.title} key={menuItem.id}>
+                  {menuItem.children.map((child) => {
+                    return (
+                      <Menu.Item>
+                        <Link
+                          to={`/categories/${child.slug}`}
+                          className="block px-4 py-2"
+                          key={child.id}
+                        >
+                          {child.title}
+                        </Link>
+                      </Menu.Item>
+                    );
+                  })}
+                </DropdownMenu>
+              );
+            })}
           </div>
         </ContainerFixed>
       </div>
