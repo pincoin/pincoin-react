f490f0c0b16ef5129dd982d5a32a2e558e4bf0e9
pincoin <pincoins@gmail.com>
2023-08-11 16:00:05 +0900

    drawer left to right

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -29,16 +29,16 @@ const Drawer = (props) => {
           <Transition.Child
             as={Fragment}
             enter="transform transition ease-in-out duration-100"
-            enterFrom="translate-x-full"
+            enterFrom="-translate-x-full" /* left to right */
             enterTo="translate-x-0"
             leave="transform transition ease-in-out duration-100"
             leaveFrom="translate-x-0"
-            leaveTo="translate-x-full"
+            leaveTo="-translate-x-full" /* left to right */
           >
             <div className="fixed inset-0 overflow-hidden">
               <div className="absolute inset-0 overflow-hidden">
-                <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-40">
-                  {/* 좌측 여백으로 크기 조정 */}
+                <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pr-32">
+                  {/* 우측 여백으로 크기 조정 */}
                   <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
                     <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
                       <div className="px-4 sm:px-6">
