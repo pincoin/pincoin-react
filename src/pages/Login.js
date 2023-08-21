93df7b89239aba4b0fbaece158744c6472be830c
pincoin <pincoins@gmail.com>
2023-08-21 11:12:10 +0900

    sns buttons

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,27 +5,37 @@ import ContainerFixed from '../ui/layouts/ContainerFixed';
 const Login = () => {
   return (
     <ContainerFixed className="px-2 py-4">
-      <div className="flex flex-col mx-auto md:w-1/2 gap-y-4">
+      <div className="flex flex-col mx-auto md:w-1/2 gap-y-4 md:shadow-md md:p-4">
         <p className="flex-1 leading-relaxed bg-amber-100 rounded px-4 py-2">
           1년 이상 미접속 고객님은 다시 회원가입해주세요.
           <br /> [정보통신망법]에 따라 1년 이상 미접속 계정 정보는 모두
           파기합니다.
         </p>
 
-        <form className="flex-1 flex flex-col">
+        <form className="flex-1 flex flex-col gap-y-4">
           <div className="flex-1">
             <input type="text" placeholder="이메일주소" />
           </div>
-          <div>
+          <div className="flex-1">
             <input type="password" placeholder="비밀번호" />
           </div>
-          <div>
-            <button type="submit">로그인</button>
+          <div className="flex-1">
+            <button type="submit" className="w-full p-2 bg-green-950 text-white rounded">로그인</button>
           </div>
         </form>
-        <div className="flex-1 flex flex-col md:flex-row gap-x-4 gap-y-4 pb-2">
+
+        <div className="relative w-10/12 md:w-1/2 mx-auto">
+          <div className="absolute inset-0 flex items-center">
+            <div className="w-full border-t border-gray-200"></div>
+          </div>
+          <div className="relative flex justify-center">
+            <span className="bg-white px-8 text-gray-500 text-sm">SNS 로그인</span>
+          </div>
+        </div>
+
+        <div className="flex-1 flex flex-col md:flex-row gap-x-4 gap-y-4">
           <div className="flex-1 flex order-2 md:order-1">
-            <div className="flex gap-x-2 mx-auto">
+            <div className="flex gap-x-8 mx-auto">
               <Link to="" className="">
                 <img
                   src={'/assets/naver.png'}
@@ -56,20 +66,17 @@ const Login = () => {
               </Link>
             </div>
           </div>
-          <div className="flex-1 order-1 md:order-2 text-center my-auto">
-            <h1 className="font-bold text-lg">SNS 간편로그인</h1>
-          </div>
         </div>
         <div className="flex-1 flex flex-col md:flex-row gap-x-4 gap-y-4">
           <Link
             to=""
-            className="flex-1 text-center bg-teal-600 rounded py-2 text-white"
+            className="flex-1 text-center bg-green-50 rounded py-2"
           >
             비밀번호찾기
           </Link>
           <Link
             to=""
-            className="flex-1 text-center bg-emerald-600 rounded py-2 text-white"
+            className="flex-1 text-center bg-green-50 rounded py-2"
           >
             회원가입
           </Link>
