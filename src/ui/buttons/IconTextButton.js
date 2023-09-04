029fd02d009fbfe7f584fcd9c0f87d400707ce24
pincoin <pincoins@gmail.com>
2023-09-04 17:37:34 +0900

    button styles

--- src/ui/buttons/IconTextButton.js
+++ src/ui/buttons/IconTextButton.js
@@ -1,11 +1,39 @@
+import className from 'classnames';
 import React from 'react';
 
-const IconTextButton = ({ children, ...rest }) => {
+const IconTextButton = ({
+  children,
+  loading,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded,
+  ...rest
+}) => {
+  const classes = className(
+    rest.className,
+    'inline-flex items-center gap-x-1',
+    {
+      'border-blue-500 bg-blue-500 text-white': primary,
+      'border-gray-900 bg-gray-900 text-white': secondary,
+      'border-green-500 bg-green-500 text-white': success,
+      'border-yellow-400 bg-yellow-400 text-white': warning,
+      'border-red-500 bg-red-500 text-white': danger,
+      'rounded-full': rounded,
+      'bg-white': outline,
+      'text-blue-500': outline && primary,
+      'text-gray-900': outline && secondary,
+      'text-green-500': outline && success,
+      'text-yellow-400': outline && warning,
+      'text-red-500': outline && danger,
+    }
+  );
+
   return (
-    <button
-      className={`${rest.className ? rest.className : ''} inline-flex items-center gap-x-1`}
-      {...rest}
-    >
+    <button className={classes} disabled={loading} {...rest}>
       {children}
     </button>
   );
--- src/ui/buttons/TextButton.js
+++ src/ui/buttons/TextButton.js
@@ -1,13 +1,39 @@
+import className from 'classnames';
 import React from 'react';
 
-const TextButton = ({ children, ...rest }) => {
+const TextButton = ({
+  children,
+  loading,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded,
+  ...rest
+}) => {
+  const classes = className(
+    rest.className,
+    'focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2',
+    {
+      'border-blue-500 bg-blue-500 text-white': primary,
+      'border-gray-900 bg-gray-900 text-white': secondary,
+      'border-green-500 bg-green-500 text-white': success,
+      'border-yellow-400 bg-yellow-400 text-white': warning,
+      'border-red-500 bg-red-500 text-white': danger,
+      'rounded-full': rounded,
+      'bg-white': outline,
+      'text-blue-500': outline && primary,
+      'text-gray-900': outline && secondary,
+      'text-green-500': outline && success,
+      'text-yellow-400': outline && warning,
+      'text-red-500': outline && danger,
+    }
+  );
+
   return (
-    <button
-      className={`${
-        rest.className ? rest.className : ''
-      } rounded focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2`}
-      {...rest}
-    >
+    <button className={classes} disabled={loading} {...rest}>
       {children}
     </button>
   );
